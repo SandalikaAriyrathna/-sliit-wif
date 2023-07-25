b9969ef2a63203dc6bc970b105397f05c6784e5d
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-07-25 21:29:14 +0530

    updated

--- my-project/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-
-    return (
-        <button className="animated-chevron splash animated-border">Custom Button</button>
-
-    )
-}
-
-export default App
--- react-tailwind-anim-btn-plugin/src/Button.jsx
+++ react-tailwind-anim-btn-plugin/src/Button.jsx
@@ -1,8 +1,10 @@
+import './colors.css';
+
 // eslint-disable-next-line react/prop-types
 const Button = ({ children, onClick }) => {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-600 rounded-[20px] text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline animated-border from-red-600 via-red-400 to-blue-500 transition-all"
+      className="w-2/12 bg-blue-400 hover:bg-blue-100 hover:text-blue-400 rounded-[20px] text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline animated-border from-red-600 via-red-400 to-blue-500 transition-all"
       onClick={onClick}
     >
       {children}
